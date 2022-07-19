b5afc5331018167323e3b4904aaceeda735b80f3
Olarealcolly <olaoluwaoyeyipo@yahoo.com>
2022-07-18 17:06:15 -0700

    ola

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,31 +1,69 @@
 #include "sort.h"
-#include <stdlib.h>
-
 /**
- * pow_10 - calculates a positive power of 10
- * @power: power of 10 to calculate
- *
- * Return: the corresponding power of 10
+ * max_val - gets max value from array
+ * @array: pointer to array
+ * @size: size of the array
+ * Return: max value from array
  */
-unsigned int pow_10(unsigned int power)
+int max_val(int *array, size_t size)
 {
-	unsigned int i, result;
+	int max = array[0];
+	size_t i;
 
-	result = 1;
-	for (i = 0; i < power; i++)
-		result *= 10;
-	return (result);
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	return (max);
 }
-
 /**
- * count_sort - sorts an array of integers in ascending order at a specific
- * digit location using the Counting sort algorithm
- * @array: array to sort
- * @size: size of the array to sort
- * @digit: digit to sort by
- *
- * Return: 1 if there is a need to keep sorting, 0 if not
+ * radix_sort - sorts an array of integers is ASC
+ * order implementing Radix Sort algorithm
+ * @array: pointer to array
+ * @size: size of the array
  */
-unsigned int count_sort(int *array, size_t size, unsigned int digit)
+void radix_sort(int *array, size_t size)
 {
+	int *new_arr;
+	int i, max, e = 1;
+	int tam = size;
+
+	if (!array || size < 2)
+		return;
+
+	max = max_val(array, size);
+
+	new_arr = malloc(sizeof(int) * size);
+	while (max / e > 0)
+	{
+		int brews[20] = {0};
+
+		i = 0;
+
+		while (i < tam)
+		{
+			brews[(array[i] / e) % 10]++;
+			i++;
+		}
+
+		if (brews != NULL)
+		{
+			for (i = 1; i < 10; i++)
+				brews[i] += brews[i - 1];
+
+			for (i = tam - 1; i >= 0; i--)
+			{
+				new_arr[brews[(array[i] / e) % 10] - 1] = array[i];
+				brews[(array[i] / e) % 10]--;
+			}
+
+			for (i = 0; i < tam; i++)
+				array[i] = new_arr[i];
+		}
+		e *= 10;
+		print_array(array, size);
+	}
+	free(new_arr);
+}
 
